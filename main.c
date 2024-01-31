21dbe1a009fac20e6adc71d1a48e5a0952f416fa
Rafael Santos <rafa@blackmagicbox.com.br>
2024-01-31 23:42:16 +0100

    FEAT Capitalise text

--- main.c
+++ main.c
@@ -5,22 +5,12 @@ int main (void) {
 
   printf("Hello world!\n");
   while ((ch = getchar()) != EOF) {
-    switch(ch) {
-      case 'a':
-      case 'A':
-      case 'e':
-      case 'E':
-      case 'i':
-      case 'I':
-      case 'o':
-      case 'O':
-      case 'u':
-      case 'U':
-        putchar('*');
-        break;
-      default:
-        putchar(ch);
+    if (ch >= 'a' && ch <= 'z') {
+      ch += ('A' - 'a');
+      putchar(ch);
+      continue;
     }
+    putchar(ch);
   }
 
   return 0;
