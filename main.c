8b7235cf302fafd55bbbb830b78c9be299bbfef7
Rafael Santos <rafa@blackmagicbox.com.br>
2024-02-02 00:59:13 +0100

    FEAT A to D Shift method and paramter selection of encryption algorithm

--- main.c
+++ main.c
@@ -1,11 +1,46 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
 #include <ctype.h>
+#include <string.h>
 
-int main(void) {
+#define NUMBER_OF_LETTERS_IN_THE_ALPHABET 26
+
+int rot13(void);
+
+int aToDShift(void);
+
+int main(int argc, char **argv) {
+  if (argc > 1) {
+    // Check if the second argument is rot13 or not
+    if (strcmp(argv[1], "rot13") == 0) {
+      rot13();
+    }
+  } else {
+    aToDShift();
+  }
+}
+
+int aToDShift(void) {
+  int shift, ch;
+
+  shift = 'D' - 'A';
+
+  while((ch = getchar())!= EOF) {
+    if(isalpha(ch)) {
+      ch += shift;
+      if((ch > 'Z' && ch < 'a') || (ch > 'z')) {
+        ch -= NUMBER_OF_LETTERS_IN_THE_ALPHABET;
+      }
+    }
+    putchar(ch);
+  }
+  putchar('\n');
+  return 0;
+}
+
+
+int rot13(void) {
   int ch;
-  srand((unsigned) time(NULL));
 
   while ((ch = getchar()) != EOF) {
     if (isalpha(ch)) {
