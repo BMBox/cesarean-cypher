fce7c6ab207f2002e4b484e389f969b73e74d8b6
Rafael Santos <rafa@blackmagicbox.com.br>
2024-02-03 00:09:47 +0100

    CHORE: Improve readability and remove duplicated code

--- main.c
+++ main.c
@@ -3,14 +3,11 @@
 #include <string.h>
 
 #define NUMBER_OF_LETTERS_IN_THE_ALPHABET 26
+#define ROT13_SHIFT 13
 
 void print_usage(char *program_name);
 
-void shift_text(int shift, int direction);
-
-void shift_text_with_offset(char offset, int modifier);
-
-void rot13(void);
+void shift_text(char offset, int modifier, int default_shift);
 
 int main(int argc, char **argv) {
   if (argc < 2) {
@@ -19,10 +16,10 @@ int main(int argc, char **argv) {
   }
 
   if (strcmp(argv[1], "-rot") == 0) {
-    rot13();
+    shift_text(0, 0, ROT13_SHIFT);
   } else if (isalpha(*argv[1])) {
     int modifier = argc > 2 && strcmp(argv[2], "-d") == 0;
-    shift_text_with_offset(*argv[1], modifier);
+    shift_text(*argv[1], modifier, 0);
   } else {
     print_usage(argv[0]);
     return 1;
@@ -35,8 +32,13 @@ void print_usage(char *program_name) {
   printf("Usage: %s <-rot| offset{a-z|A-Z}> [modifier{-d}]\n", program_name);
 }
 
-void shift_text(int shift, int direction) {
+void shift_text(char offset, int modifier, int default_shift) {
   int ch;
+  int shift = default_shift;
+  if (offset != 0) {
+    shift = modifier == 1 ? 'A' - toupper(offset) : toupper(offset) - 'A';
+  }
+  int direction = modifier == 1 ? 1 : -1;
 
   int trim_out_of_bounds = NUMBER_OF_LETTERS_IN_THE_ALPHABET * direction;
   while ((ch = getchar()) != EOF) {
@@ -49,14 +51,4 @@ void shift_text(int shift, int direction) {
     putchar(ch);
   }
   putchar('\n');
-}
-
-void shift_text_with_offset(char offset, int modifier) {
-  int shift = modifier == 1 ? 'A' - toupper(offset) : toupper(offset) - 'A';
-  int direction = modifier == 1 ? 1 : -1;
-  shift_text(shift, direction);
-}
-
-void rot13(void) {
-  shift_text(13, 1);
 }
