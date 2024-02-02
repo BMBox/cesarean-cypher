5da60901c5cc53e00ccf19d8a8ca165fdf3c6e27
Rafael Santos <rafa@blackmagicbox.com.br>
2024-02-02 22:57:12 +0100

    FEAT Cypher method taking an offset and an optional modifier to decrypt

--- main.c
+++ main.c
@@ -11,20 +11,31 @@ int aToDShift(void);
 
 int decryptAToD(void);
 
+int cypher(int offset, int modifier);
+
 int main(int argc, char **argv) {
-  if (argc > 1) {
-    // Check if the second argument is rot13 or not
-    if (strcmp(argv[1], "-r") == 0) {
-      rot13();
-    } else if (strcmp(argv[1], "-d") == 0) {
-      decryptAToD();
+  if (argc > 2) {
+    int modifier = strcmp(argv[2], "-d") == 0;
+    if (isalpha(*argv[1]) && modifier == 1) {
+      cypher(*argv[1], modifier);
     } else {
-      printf("Usage %s <-r|-d> \n", argv[0]);
-      exit(EXIT_FAILURE);
+      printf("Usage: %s <-rot|-decr| offset{a-z|A-Z}> <modifier{-d}>", argv[0]);
     }
+  } else if (argc > 1) {
+    if (strcmp(argv[1], "-rot") == 0) {
+      rot13();
+    } else if (strcmp(argv[1], "-decr") == 0) {
+      decryptAToD();
+    } else if (isalpha(*argv[1])) {
+      cypher((*argv[1]), 0);
+    } else {
+      printf("Usage: %s <-rot|-decr|{a-z|A-Z}", argv[0]);
+    }
+
   } else {
     aToDShift();
   }
+  return 0;
 }
 
 int aToDShift(void) {
@@ -58,6 +69,31 @@ int decryptAToD(void) {
       }
     }
     putchar(ch);
+  }
+  putchar('\n');
+
+  return 0;
+}
+
+
+int cypher(int offset, int modifier) {
+  int ch;
+  int shift = modifier == 1 ? 'A' - toupper(offset) : toupper(offset) - 'A';
+
+  while ((ch = getchar()) != EOF) {
+    if (isupper(ch)) {
+      ch += shift;
+      if (ch > 'Z') ch -= NUMBER_OF_LETTERS_IN_THE_ALPHABET;
+      if (ch < 'A') ch += NUMBER_OF_LETTERS_IN_THE_ALPHABET;
+      putchar(ch);
+    } else if (islower(ch)) {
+      ch += shift;
+      if (ch > 'z') ch -= NUMBER_OF_LETTERS_IN_THE_ALPHABET;
+      if (ch < 'a') ch += NUMBER_OF_LETTERS_IN_THE_ALPHABET;
+      putchar(ch);
+    } else {
+      putchar(ch);
+    }
   }
   putchar('\n');
 
